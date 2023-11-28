435d64aafa2fb070e67d0793760524bba1af8746
HaiderMontes <147106881+HaiderMontes@users.noreply.github.com>
2023-11-28 15:47:08 -0500

    organice vistas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,13 @@
 <template>
-    <div class="body">
-        <div>
-            <Navbar />
-            <div class="titulo_tarea">
-                <h1>AGENDAR TAREA</h1>
-            </div>
-            <Formulario />
-        </div>
+    <div>
+        <Login />
     </div>
 </template>
 
 <script setup lang="ts">
-import Navbar from '@/components/Navbar.vue';
-import Formulario from '@/components/Formulario.vue';
-
-
+import Login from '@/components/Login.vue';
 </script>
 
 <style scoped>
-.titulo_tarea {
-    text-align: center;
-    background-image: url(../assets/fondo.png);
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-attachment: fixed;
-    margin: 4px;
-    color: white;
-}
 
-.body {
-    width: 100%;
-    height: 735px;
-    /* background-color: #9fa8da; */
-}
 </style>
