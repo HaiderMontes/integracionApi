877e18b7b8bdabe4167be655b935b8e166ffe823
tatianaDelRio <tatianadelriocarmona@gmail.com>
2023-11-28 17:51:49 -0500

    actualizaciones del login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,9 +2,9 @@
      <div class="body">
        <div class="container">
          <div class="signin-signup">
-           <div class="form-container" :class="{ 'form-left': !isSignInForm, 'form-right': isSignInForm }">
+           <div class="form-container separar" :class= "{ 'form-left': !isSignInForm, 'form-right': isSignInForm }">
              <form v-if="isSignInForm" action="" class="sig-in-form">
-               <h2 class="tittle">login</h2>
+               <h2 class="tittle">Login</h2>
                <div class="input-field">
                               <i class="fas fa-user"></i>
                               <input type="text" placeholder="Username">
@@ -14,13 +14,13 @@
                               <input type="password" placeholder="password">
                          </div>
 
-               <button @click="toggleForm" type="button" class="btn btn-outline" id="sig-in-btn">Sign In</button>
+               <button @click="toggleForm" type="button" class="boton btn btn-outline" id="sig-in-btn">STAR</button>
              </form>
-             <form v-else action="" class="sign-up-form">
-               <h2 class="tittle">sign up</h2>
-               <div class="input-field">
+             <form v-else action="" class="separador sign-up-form">
+               <h2 class="tittle">Sign up</h2>
+               <div class="sinput-field">
                               <i class="fas fa-user"></i>
-                              <input class="color" type="text" placeholder="Username">
+                              <input class=" color" type="text" placeholder="Username">
                          </div>
                          <div class="input-field">
                               <i class="fas fa-lock"></i>
@@ -42,12 +42,12 @@
              <div class="panel left-panel" v-if="isSignInForm">
                <!-- Contenido del panel izquierdo para Sign In -->
                <p class="color">El tiempo es más valioso que el <br> dinero, puedes conseguir más <br> dinero pero no puedes conseguir <br> más tiempo</p>
-               <button @click="toggleForm" type="button" class="btn btn-outline" id="sig-in-btn">Sign In</button>
+               <button @click="toggleForm" type="button" class="boton btn btn-outline" id="sig-in-btn">REGISTER</button>
              </div>
              <div class="panel right-panel" v-else>
                <!-- Contenido del panel derecho para Sign Up -->
-               <p class="color">Una buena vida se basa en <br> ser un buen gestor del <br> tiempo.</p>
-               <button @click="toggleForm" type="button" class="btn btn-outline" id="sig-up-btn">Sign Up</button>
+               <p class="tcolor">Una buena vida se basa en <br> ser un buen gestor del <br> tiempo.</p>
+               <button @click="toggleForm" type="button" class="btn btn btn-outline" id="sig-up-btn">STAR</button>
              </div>
            </div>
          </div>
@@ -103,6 +103,8 @@
    .signin-signup {
      display: flex;
      justify-content: space-around;
+     color: antiquewhite;
+    
    }
    
    .form-container {
@@ -130,14 +132,57 @@
    
    input{
      background-color: #030A1C;
-     color: white;
      border-radius: 0.5em;
+     margin: 11px;
+     text-align: center;
+     width: 300px;
+     height: 40px;
+     margin-left: 35px;
+     color: #fff;
 
    }
+
    .color{
      color: white;
+     display: flex;
+     margin: 60px;
+     
+   }
+   .tcolor{
+     color: white;
+     display: flex;
+     margin: 100px;
+     
+   }
+   .separar{
+    border-left: 1px solid #fff;
    }
 
+   .separador{
+    border-right: 1px solid #fff;
+   }
+
+  
+   .tittle{
+    border-bottom: 1px solid #fff;
+    margin: 30px;
+
+   }
+
+   .boton{
+    margin-left: 150px;
+    background-color: #030A1C;
+    width: 100px;
+    height: 40px;
+    }
+
+    .btn{
+      background-color: #030A1C;
+      margin-left: 150px;
+      width: 100px;
+      height: 40px;
+      
+    }
    
    
    </style>
